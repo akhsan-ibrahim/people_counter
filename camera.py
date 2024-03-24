9d8ae1032a23d2b62adda9a87ae4791d27d08591
Akhsan Ibrahim <akhsanibra08@gmail.com>
2024-03-24 23:45:25 +0700

    detection person

--- camera.py
+++ camera.py
@@ -1,12 +1,27 @@
 import cv2
+from ultralytics import YOLO
 
 class Camera():
    def __init__(self):
       self.cap = cv2.VideoCapture(0)
+      self.model = YOLO("yolov8n.pt")
       
    def get_frame(self):
       """Get video frame"""
       ret, frame = self.cap.read()
+      
+      results = self.model.predict(frame, classes=0, stream_buffer=True, stream=True)
+      for result in results:
+         detections = []
+         for r in result.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = r
+            x1 = int(x1)
+            y1 = int(y1)
+            x2 = int(x2)
+            y2 = int(y2)
+            class_id = int(class_id)
+            detections.append([x1, y1, x2, y2, score])
+            cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,255), 2)
       
       ret, jpeg = cv2.imencode(".jpg", frame) # Convert frame to jpeg image
       return jpeg.tobytes() # Convert jpeg to byte for send to web browser
