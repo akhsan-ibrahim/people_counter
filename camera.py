041ce84d1af5ba986d7ce57d18f2564485dd5f6c
Akhsan Ibrahim <akhsanibra08@gmail.com>
2024-03-25 13:12:20 +0700

    counting zone

--- camera.py
+++ camera.py
@@ -2,14 +2,18 @@ import cv2
 from ultralytics import YOLO
 from tracker import Tracker
 import random as rd
+import numpy as np
 
 class Camera():
    def __init__(self):
       # self.cap = cv2.VideoCapture(0)
-      self.cap = cv2.VideoCapture("rtsp://admin:admin123@192.168.195.167:554/Streaming/Channels/501")
+      # self.cap = cv2.VideoCapture("rtsp://admin:admin123@192.168.195.167:554/Streaming/Channels/501")
+      self.cap = cv2.VideoCapture("student.mp4")
       self.model = YOLO("yolov8n.pt")
       self.tracker = Tracker()
       self.colors = [(rd.randint(0, 255), rd.randint(0, 255), rd.randint(0, 255)) for i in range(10)]
+      # self.polygon_points = [[500,400], [500+300,400], [500+300,400+100], [500,400+100]]
+      self.polygon_points = [(494+20,289+120), (505+20,499+120), (578+20,496+120), (530+20,292+120)]
       
    def get_frame(self):
       """Get video frame"""
@@ -39,6 +43,10 @@ class Camera():
             
             cv2.rectangle(frame, (x1,y1), (x2,y2), self.colors[person_id % len(self.colors)], 3)
             cv2.putText(frame, str(person_id), (x1,y1), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+      
+      cv2.polylines(frame, [np.array(self.polygon_points,np.int32)], True, (0,0,0), 1)
+      cv2.line(frame, (self.polygon_points[0]), (self.polygon_points[1]), (255,0,0), 3)
+      cv2.line(frame, (self.polygon_points[2]), (self.polygon_points[3]), (0,255,0), 3)
       
       ret, jpeg = cv2.imencode(".jpg", frame) # Convert frame to jpeg image
       return jpeg.tobytes() # Convert jpeg to byte for send to web browser
