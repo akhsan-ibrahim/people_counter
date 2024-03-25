d92ffb4d4d3cdb91507331731e7a892a0569749e
Akhsan Ibrahim <akhsanibra08@gmail.com>
2024-03-25 09:50:06 +0700

    tracking person

--- camera.py
+++ camera.py
@@ -1,10 +1,15 @@
 import cv2
 from ultralytics import YOLO
+from tracker import Tracker
+import random as rd
 
 class Camera():
    def __init__(self):
-      self.cap = cv2.VideoCapture(0)
+      # self.cap = cv2.VideoCapture(0)
+      self.cap = cv2.VideoCapture("rtsp://admin:admin123@192.168.195.167:554/Streaming/Channels/501")
       self.model = YOLO("yolov8n.pt")
+      self.tracker = Tracker()
+      self.colors = [(rd.randint(0, 255), rd.randint(0, 255), rd.randint(0, 255)) for i in range(10)]
       
    def get_frame(self):
       """Get video frame"""
@@ -21,7 +26,19 @@ class Camera():
             y2 = int(y2)
             class_id = int(class_id)
             detections.append([x1, y1, x2, y2, score])
-            cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,255), 2)
+            
+         self.tracker.update(frame, detections)
+         for track in self.tracker.tracks:
+            bbox = track.bbox
+            x1, y1, x2, y2 = bbox
+            x1 = int(x1)
+            y1 = int(y1)
+            x2 = int(x2)
+            y2 = int(y2)
+            person_id = track.track_id
+            
+            cv2.rectangle(frame, (x1,y1), (x2,y2), self.colors[person_id % len(self.colors)], 3)
+            cv2.putText(frame, str(person_id), (x1,y1), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
       
       ret, jpeg = cv2.imencode(".jpg", frame) # Convert frame to jpeg image
       return jpeg.tobytes() # Convert jpeg to byte for send to web browser
